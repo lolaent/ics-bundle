fef6fc1795b2c4a406c0691848e517c584eab740
Georgiana Gligor <g@lolaent.com>
2015-03-13 16:55:48 +0200

    Symfony ICS bundle skeleton * complete with test setup

---
+++ IcsBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cti\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/**
+ * IcsBundle
+ *
+ * @package CTI\Bundle\IcsBundle
+ */
+class IcsBundle extends Bundle
+{
+}
---
+++ Tests/app/AppKernel.php
@@ -0,0 +1,27 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class AppKernel to be used as an application kernel in our tests
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return array(
+            new \Cti\Bundle\IcsBundle(),
+        );
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config.yml');
+    }
+}
